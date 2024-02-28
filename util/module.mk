a7a7034fdc14e0b5ffb2ba9721f33496ff733336
Michael C. Lehn <michel.lehn@uni-ulm.de>
2024-02-28 19:42:44 +0100

    Added missed util/

---
+++ util/module.mk
@@ -0,0 +1,3 @@
+# All files in source directory beginning with 'xtest' are optional targets
+# (built with 'make opt'). Here we here default targets (built with 'make').
+
---
+++ util/ustr.hpp
@@ -0,0 +1,78 @@
+#ifndef LEXER_USTR_HPP
+#define LEXER_USTR_HPP
+
+#include <cstdint>
+#include <functional>
+#include <ostream>
+#include <set>
+#include <string>
+
+namespace abc {
+
+class UStr
+{
+    public:
+	UStr();
+	UStr(const UStr &) = default;
+
+    protected:	
+	UStr(const std::string &s);
+
+    public:
+
+	static UStr create(const char *s);
+	static UStr create(const std::string &s);
+
+	UStr &operator=(const UStr &) = default;
+	UStr &operator=(UStr &&) = default;
+
+	const char *
+	c_str() const
+	{
+	    return c_str_;
+	}
+
+	std::size_t
+	length() const
+	{
+	    return len;
+	}
+
+    private:
+	const char *c_str_;
+	std::size_t len;
+};
+
+std::ostream &operator<<(std::ostream &out, const UStr &ustr);
+
+inline bool
+operator==(const UStr &a, const UStr &b)
+{
+    return a.c_str() == b.c_str();
+}
+
+inline bool
+operator!=(const UStr &a, const UStr &b)
+{
+    return a.c_str() != b.c_str();
+}
+
+inline bool
+operator<(const UStr &a, const UStr &b)
+{
+    return a.c_str() < b.c_str();
+}
+
+} // namespace abc
+
+template<>
+struct std::hash<abc::UStr>
+{
+    std::size_t operator()(const abc::UStr& s) const noexcept
+    {
+        return std::hash<const char *>{}(s.c_str());
+    }
+};
+
+
+#endif // LEXER_USTR_HPP
