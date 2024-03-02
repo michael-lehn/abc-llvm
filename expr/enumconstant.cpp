55eb6b86110e6e4d7b6e352ce49b66c940c5560f
Michael C. Lehn <michel.lehn@uni-ulm.de>
2024-03-02 20:43:55 +0100

    Added missed files

---
+++ expr/enumconstant.cpp
@@ -0,0 +1,94 @@
+#include <charconv>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+
+#include "gen/constant.hpp"
+#include "gen/instruction.hpp"
+#include "lexer/error.hpp"
+#include "type/integertype.hpp"
+
+#include "enumconstant.hpp"
+
+//------------------------------------------------------------------------------
+
+namespace abc {
+
+EnumConstant::EnumConstant(UStr name, std::int64_t value, const Type *type,
+			   lexer::Loc loc)
+    : Expr{loc, type}, name{name}, value{value}
+{
+}
+
+ExprPtr
+EnumConstant::create(UStr name, std::int64_t value, const Type *type,
+		     lexer::Loc loc)
+{
+    assert(!name.empty());
+    auto p = new EnumConstant{name, value, type, loc};
+    return std::unique_ptr<EnumConstant>{p};
+}
+
+bool
+EnumConstant::hasAddress() const
+{
+    return false;
+}
+
+bool
+EnumConstant::isLValue() const
+{
+    return false;
+}
+
+bool
+EnumConstant::isConst() const
+{
+    return true;
+}
+
+// for code generation
+gen::Constant
+EnumConstant::loadConstant() const
+{
+    return gen::getConstantInt(value, type);
+}
+
+gen::Value
+EnumConstant::loadValue() const
+{
+    return loadConstant();
+}
+
+gen::Value
+EnumConstant::loadAddress() const
+{
+    assert(0 && "EnumConstant has no address");
+    return nullptr;
+}
+
+void
+EnumConstant::condition(gen::Label trueLabel, gen::Label falseLabel) const
+{
+    auto zero = gen::getConstantZero(type);
+    auto cond = gen::instruction(gen::NE, loadValue(), zero);
+    gen::jumpInstruction(cond, trueLabel, falseLabel);
+}
+
+// for debugging and educational purposes
+void
+EnumConstant::print(int indent) const
+{
+    if (indent) {
+	std::cerr << std::setfill(' ') << std::setw(indent) << ' ';
+    }
+    std::cerr << name << " [ " << value << " ] " << std::endl;
+}
+
+void
+EnumConstant::printFlat(std::ostream &out, int prec) const
+{
+    out << name;
+}
+
+} // namespace abc
---
+++ expr/enumconstant.hpp
@@ -0,0 +1,45 @@
+#ifndef ENUMCONSTANT_HPP
+#define ENUMCONSTANT_HPP
+
+#include <cstdint>
+
+#include "expr.hpp"
+#include "lexer/loc.hpp"
+
+namespace abc {
+
+class EnumConstant : public Expr
+{
+    protected:
+	EnumConstant(UStr name, std::int64_t value, const Type *type,
+		     lexer::Loc loc);
+
+    public:
+	static ExprPtr create(UStr name, std::int64_t value, const Type *type,
+			      lexer::Loc loc);		
+
+	const UStr name;
+	const std::int64_t value;
+
+	// for sematic checks
+	bool hasAddress() const override;
+	bool isLValue() const override;
+	bool isConst() const override;
+
+	// for code generation
+	gen::Constant loadConstant() const override;
+	gen::Value loadValue() const override;
+	gen::Value loadAddress() const override;
+	void condition(gen::Label trueLabel,
+		       gen::Label falseLabel) const override;
+
+	// for debugging and educational purposes
+	void print(int indent) const override;
+
+	// for printing error messages
+	void printFlat(std::ostream &out, int prec) const override;
+};
+
+} // namespace abc
+ 
+#endif // ENUMCONSTANT_HPP
