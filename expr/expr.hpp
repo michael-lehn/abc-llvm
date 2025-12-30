bf3b90e663f0b0d4db0a7991d0cf73a45a9b6ae5
Michael Lehn <michael.lehn@uni-ulm.de>
2025-12-30 19:40:37 +0100

    cleanup

--- expr/expr.hpp
+++ expr/expr.hpp
@@ -22,50 +22,35 @@ class Expr
 	const Type *const type;
 
 	// for sematic checks
-	virtual bool
-	hasConstantAddress() const;
-	virtual bool
-	hasAddress() const = 0;
-	virtual bool
-	isLValue() const = 0;
-	virtual bool
-	isConst() const = 0;
+	virtual bool hasConstantAddress() const;
+	virtual bool hasAddress() const = 0;
+	virtual bool isLValue() const = 0;
+	virtual bool isConst() const = 0;
 
 	// for code generation
-	virtual gen::Constant
-	loadConstant() const = 0;
-	virtual gen::Value
-	loadValue() const = 0;
-	virtual gen::Constant
-	loadConstantAddress() const;
-	virtual gen::Value
-	loadAddress() const = 0;
-	virtual void
-	condition(gen::Label trueLabel, gen::Label falseLabel) const;
+	virtual gen::Constant loadConstant() const = 0;
+	virtual gen::Value loadValue() const = 0;
+	virtual gen::Constant loadConstantAddress() const;
+	virtual gen::Value loadAddress() const = 0;
+	virtual void condition(gen::Label trueLabel,
+	                       gen::Label falseLabel) const;
 
 	// for debugging and educational purposes
-	virtual void
-	print(int indent = 1) const = 0;
+	virtual void print(int indent = 1) const = 0;
 
 	// for printing error messages
-	virtual void
-	printFlat(std::ostream &out, int prec) const = 0;
+	virtual void printFlat(std::ostream &out, int prec) const = 0;
 
 	// get value from const expressions with integer type
-	gen::ConstantInt
-	getConstantInt() const;
-	std::int64_t
-	getSignedIntValue() const;
-	std::uint64_t
-	getUnsignedIntValue() const;
+	gen::ConstantInt getConstantInt() const;
+	std::int64_t getSignedIntValue() const;
+	std::uint64_t getUnsignedIntValue() const;
 };
 
 using ExprPtr = std::unique_ptr<const Expr>;
 
-std::ostream &
-operator<<(std::ostream &out, const ExprPtr &expr);
-std::ostream &
-operator<<(std::ostream &out, const Expr *expr);
+std::ostream &operator<<(std::ostream &out, const ExprPtr &expr);
+std::ostream &operator<<(std::ostream &out, const Expr *expr);
 
 } // namespace abc
 
