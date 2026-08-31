8634f90a8fe9cdf65a9e4474ce3d13300e3762be
Michael Lehn <michael.lehn@uni-ulm.de>
2026-08-31 23:36:07 +0200

    Add initial ABI support and LLVM 17–22 compatibility

---
+++ chk_llvm_version.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+versions="17 18 19 20 21 22"
+
+rm -f chk_llvm.log
+
+for version in $versions; do
+    make clean
+
+    if make -j 8 \
+        llvm-config=/usr/local/opt/llvm@${version}/bin/llvm-config \
+        install && \
+	(cd raylib-example && make clean && make) && \
+	(cd not-abc && make clean && make)
+    then
+        echo "${version} ok" >> chk_llvm.log
+    else
+        echo "${version} failed" >> chk_llvm.log
+    fi
+done
+
+cat chk_llvm.log
